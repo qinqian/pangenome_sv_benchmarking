73811776010f1bda84dbd837bd2cb2e58289805c
qinqian <qqin@broadinstitute.org>
2024-02-25 00:16:35 +0000

    add clean up scripts

---
+++ 0.alignment/minimap2_cram_to_cram.wdl
@@ -0,0 +1,73 @@
+version 1.0
+
+
+workflow LineargenomeAlignment {
+    input {
+        File cram
+        File crai
+
+        String sample_id
+        File assembly
+        String docker_image="trinityctat/pbfusion:v0.3.1"
+        Int preemptible=3
+        Int boot_disk_size=20
+        Int disk_space=300
+        Int cpu = 2
+        Int mem = 16
+        String mode = "map-ont"
+    }
+
+    call minimapTask {
+        input:
+            input_bam=bam,
+            sample_id=sample_id,
+            docker_image=docker_image,
+            assembly=assembly,
+            preemptible=preemptible,
+            boot_disk_size=boot_disk_size,
+            disk_space=disk_space,
+            cpu=cpu,
+            mem=mem,
+            mode=mode
+    }
+
+    output {
+        File cram = minimapTask.cram
+        File crai = minimapTask.crai
+    }
+}
+
+
+task minimapTask {
+    input {
+        File input_cram
+        File input_crai
+        String mode
+        String sample_id
+        File assembly
+        String docker_image
+        Int preemptible=3
+        Int boot_disk_size=10
+        Int disk_space=20
+        Int cpu = 10
+        Int mem = 32
+    }
+
+    command <<<
+        samtools fasta -@ ~{cpu} --reference ~{input_cram_reference} ~{input_cram} | minimap2 -ax ~{mode} -t ~{cpu} ~{assembly} - | samtools sort --write-index -l 1 -@ ~{cpu} --output-fmt CRAM --reference ~{assembly} > ~{sample_id}.cram
+    >>>
+
+    runtime {
+        disks: "local-disk ~{disk_space} HDD"
+        memory: "~{mem} GB"
+        cpu: cpu
+        preemptible: preemptible
+        bootDiskSizeGb: boot_disk_size
+        docker: docker_image
+    }
+
+    output {
+        File cram = "~{sample_id}.cram"
+        File crai = "~{sample_id}.cram.crai"
+    }
+}
--- 3.terra_submissions/dalmatian_batch_submission.py
+++ 3.terra_submissions/dalmatian_batch_submission.py
@@ -2,6 +2,7 @@ from mgenepy import terra
 import dalmatian as dm
 from depmap_omics_upload.mgenepy import terra as terra_cleanup
 from mgenepy.utils import helper as h 
+import subprocess
 
 pangenome_sv_workspace = "broad-firecloud-ccle/pangenome"
 wm = dm.WorkspaceManager(pangenome_sv_workspace)
@@ -15,7 +16,19 @@ minigraph_cram_output_to_assemblies = {"minigraph_chm13v2_gaf": "\"gs://fc-secur
                                        "minigraph_grch37graph_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/GRCh37-91c.r559.gfa.gz\"",
                                        "minigraph_grch37linear_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/hs37d5.fa\""}
 #TODO: automatically generate this in the future
-remaining_task = ["minigraph_chm13graph_gaf", "minigraph_grch38graph_gaf", "minigraph_grch37linear_gaf"]
+#remaining_task = ["minigraph_chm13graph_gaf", "minigraph_grch38graph_gaf", "minigraph_grch37linear_gaf"]
+
+
+# Workspace column name to corresponding assembly
+# This minigraph_cram_to_cram will output pafs
+minigraph_cram_output_to_assemblies = {"minigraph_chm13v2_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/chm13v2.0.fa\"", 
+                                       "minigraph_chm13graph_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/chm13-90c.r518.gfa\"",
+                                       "minigraph_grch38graph_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/GRCh38-90c.r518.gfa\"",
+                                       "minigraph_grch38_noalt_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna\"", 
+                                       "minigraph_grch37graph_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/GRCh37-91c.r559.gfa.gz\"",
+                                       "minigraph_grch37linear_gaf": "\"gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/hs37d5.fa\""}
+#TODO: automatically generate this in the future
+#remaining_task = ["minigraph_chm13graph_gaf", "minigraph_grch38graph_gaf", "minigraph_grch37linear_gaf"]
 
 
 def submit_minigraph_gaf_jobs(sample_set_id, minigraph_wdl="minigraph_cram", use_callcache=False):
@@ -23,33 +36,43 @@ def submit_minigraph_gaf_jobs(sample_set_id, minigraph_wdl="minigraph_cram", use
     #await terra.waitForSubmission(pangenome_sv_workspace, submission_id)
 
 
-#def clean_up():
-#    if doCleanup:
-#        print("cleaning workspaces")
-#        torm = await terra_cleanup.deleteHeavyFiles("broad-firecloud-ccle/DEV_DepMap_WGS_CN")
-#        h.parrun(['gsutil rm '+i for i in torm], cores=8)
-#        terra_cleanup.removeFromFailedWorkflows("broad-firecloud-ccle/DEV_DepMap_WGS_CN", dryrun=False)
+def clean_up(to_be_clean_submission_ids, dry_run=True):
+    print("cleaning workspaces")
+    #torm = await terra_cleanup.deleteHeavyFiles("broad-firecloud-ccle/DEV_DepMap_WGS_CN")
+    #h.parrun(['gsutil rm '+i for i in torm], cores=8)
+    subprocess.call("rm -f clean.logs", shell=True)
+    for submission_id in to_be_clean_submission_ids:
+        if dry_run:
+            subprocess.call(f"gsutil du -sh gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/submissions/{submission_id} >> clean.logs", shell=True)
+        else:
+            subprocess.call(f"gsutil rm -r gs://fc-secure-062e6633-7a72-4623-9394-491e0ce6c324/submissions/{submission_id}", shell=True)
+
 
 
 def main():
     print(wm.get_samples())
     print(wm.get_sample_sets())
 
-    #for output_col, assembly in minigraph_cram_output_to_assemblies.items():
-    for output_col in remaining_task:
-        assembly = minigraph_cram_output_to_assemblies[output_col]
-        print(output_col, assembly)
-        old_config = wm.get_config("minigraph_cram")
-        print(old_config)
-        old_config["inputs"]['PangenomeAlignment.assembly'] = assembly
-        print(old_config)
-        old_config['outputs']['PangenomeAlignment.gaf'] = f'this.{output_col}'
-        new_config = wm.update_config(old_config)
-        submit_minigraph_gaf_jobs("nanopore_merged_techreps")
-        #break
+    #for output_col in remaining_task:
+    #    assembly = minigraph_cram_output_to_assemblies[output_col]
+    #    print(output_col, assembly)
+    #    old_config = wm.get_config("minigraph_cram")
+    #    print(old_config)
+    #    old_config["inputs"]['PangenomeAlignment.assembly'] = assembly
+    #    print(old_config)
+    #    old_config['outputs']['PangenomeAlignment.gaf'] = f'this.{output_col}'
+    #    new_config = wm.update_config(old_config)
+    #    submit_minigraph_gaf_jobs("nanopore_merged_techreps")
+    #    #break
 
-    status = wm.get_submission_status()
+    status = wm.get_submission_status(filter_active=False)
+    status.loc[:, 'clean_up'] = False
+    # remove all jobs that has not supported ds:Z tag correctly
+    status.iloc[-31:, -1] = True
     status.to_csv("job_status.csv")
+
+    #clean_up(status.submission_id[status.clean_up])
+    #clean_up(status.submission_id[status.clean_up], dry_run=False)
 
 
 if __name__ == "__main__":
