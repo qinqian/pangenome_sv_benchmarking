f6cfdb133196ee1bdfcfccffdd1fa6a1201f5d8a
qinqian <qqin@broadinstitute.org>
2024-02-29 04:13:26 +0000

    update hprc chm13 script

--- 2.evaluation/evaluation.sh
+++ 2.evaluation/evaluation.sh
@@ -13,22 +13,37 @@ cat truthset_somaticSVs_COLO829_hg38_sort_indel.bed | grep -v "NOT_VALIDATED" | 
 python parse_severus.py > severus_somaticSVs_COLO829_hg38.bed
 grep -E "DEL" severus_somaticSVs_COLO829_hg38.bed > severus_somaticSVs_COLO829_hg38_del.bed
 grep -E "INS" severus_somaticSVs_COLO829_hg38.bed > severus_somaticSVs_COLO829_hg38_ins.bed
-gzip severus*.bed
+gzip -f severus*.bed
 
 python parse_sniffles2.py > sniffles2_somaticSVs_COLO829_hg38.bed
 grep -E "DEL" sniffles2_somaticSVs_COLO829_hg38.bed > sniffles2_somaticSVs_COLO829_hg38_del.bed
 grep -E "INS" sniffles2_somaticSVs_COLO829_hg38.bed > sniffles2_somaticSVs_COLO829_hg38_ins.bed
-gzip sniffles*.bed
 
 python parse_sniffles2_singlesample.py > sniffles2_somaticSVs_COLO829_hg38_singlesample.bed
 grep -E "DEL" sniffles2_somaticSVs_COLO829_hg38_singlesample.bed > sniffles2_somaticSVs_COLO829_hg38_singlesample_del.bed
 grep -E "INS" sniffles2_somaticSVs_COLO829_hg38_singlesample.bed > sniffles2_somaticSVs_COLO829_hg38_singlesample_ins.bed
-gzip sniffles*.bed
+gzip -f sniffles*.bed
 
-#python parse_hprc.py > hprc_grch38_wave_indels50bp.bed
+python parse_sniffles2_singlesample_chm13.py > sniffles2_somaticSVs_COLO829_chm13_singlesample.bed
+grep -E "DEL" sniffles2_somaticSVs_COLO829_chm13_singlesample.bed > sniffles2_somaticSVs_COLO829_chm13_singlesample_del.bed
+grep -E "INS" sniffles2_somaticSVs_COLO829_chm13_singlesample.bed > sniffles2_somaticSVs_COLO829_chm13_singlesample_ins.bed
+gzip -f sniffles*.bed
+
+#python parse_hprc_grch38.py > hprc_grch38_wave_indels50bp.bed
 #awk '($5 > 0)' hprc_grch38_wave_indels50bp.bed > hprc_grch38_wave_indels50bp_ins.bed
 #awk '($5 < 0)' hprc_grch38_wave_indels50bp.bed > hprc_grch38_wave_indels50bp_del.bed
 
-python parse_hprc.py > hprc_chm13_wave_indels50bp.bed
+python parse_hprc_chm13.py > hprc_chm13_wave_indels50bp.bed
 awk '($5 > 0)' hprc_chm13_wave_indels50bp.bed > hprc_chm13_wave_indels50bp_ins.bed
 awk '($5 < 0)' hprc_chm13_wave_indels50bp.bed > hprc_chm13_wave_indels50bp_del.bed
+
+zcat sniffles2_somaticSVs_COLO829_hg38_singlesample_ins.bed.gz | bedtools intersect -v -a - -b hprc_grch38_wave_indels50bp_ins.bed > COLO829.GRCh38.pacbio.wave_filtered_sniffled.ins.bed
+zcat sniffles2_somaticSVs_COLO829_hg38_singlesample_del.bed.gz | bedtools intersect -v -a - -b hprc_grch38_wave_indels50bp_del.bed > COLO829.GRCh38.pacbio.wave_filtered_sniffled.del.bed
+gzip -c COLO829.GRCh38.pacbio.wave_filtered_sniffled.ins.bed > COLO829.GRCh38.pacbio.wave_filtered_sniffled.ins.bed.gz
+gzip -c COLO829.GRCh38.pacbio.wave_filtered_sniffled.del.bed > COLO829.GRCh38.pacbio.wave_filtered_sniffled.del.bed.gz
+
+zcat sniffles2_somaticSVs_COLO829_chm13_singlesample_ins.bed.gz | bedtools intersect -v -a - -b hprc_chm13_wave_indels50bp_ins.bed > COLO829.chm13.pacbio.wave_filtered_sniffled.ins.bed
+zcat sniffles2_somaticSVs_COLO829_chm13_singlesample_del.bed.gz | bedtools intersect -v -a - -b hprc_chm13_wave_indels50bp_del.bed > COLO829.chm13.pacbio.wave_filtered_sniffled.del.bed
+gzip -c COLO829.chm13.pacbio.wave_filtered_sniffled.ins.bed > COLO829.chm13.pacbio.wave_filtered_sniffled.ins.bed.gz
+gzip -c COLO829.chm13.pacbio.wave_filtered_sniffled.del.bed > COLO829.chm13.pacbio.wave_filtered_sniffled.del.bed.gz
+
